16c5499071d39d4ae5d5434e7f76cc7415f90b6b
mudzso <mudzso@gmail.com>
2017-03-29 14:22:37 +0200

    Implements exceptions: RegisterException NameMissing EmailMissing

---
+++ src/registerException/EmailMissing.java
@@ -0,0 +1,11 @@
+package registerException;
+
+/**
+ * Created by Mudzso on 2017.03.29..
+ */
+public class EmailMissing extends RegisterException {
+
+    public EmailMissing(String message) {
+        super(message);
+    }
+}
---
+++ src/registerException/NameMissing.java
@@ -0,0 +1,11 @@
+package registerException;
+
+/**
+ * Created by Mudzso on 2017.03.29..
+ */
+public class NameMissing extends RegisterException {
+
+    public NameMissing(String message) {
+        super(message);
+    }
+}
