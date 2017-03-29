bac94743d0f52a5a99d620155e5b33a72dff99c0
imre_meszesan <imre.meszesan@gmail.com>
2017-03-29 14:40:25 +0200

    implemented RegistrationHandler: - skeleton class and empty doPost method

---
+++ src/RegistrationHandler.java
@@ -0,0 +1,18 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by imre_meszesan on 29.03.17.
+ */
+@WebServlet(name = "RegistrationHandler")
+public class RegistrationHandler extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
