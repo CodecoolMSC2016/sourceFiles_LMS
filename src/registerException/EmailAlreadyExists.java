9d298ab31ef405fb211d0e23085316a45d608840
Trixi <bozso.beatrix@gmail.com>
2017-03-30 10:41:51 +0200

    Exception class name modified

---
+++ src/registerException/EmailAlreadyExists.java
@@ -0,0 +1,11 @@
+package registerException;
+
+/**
+ * Created by Mudzso on 2017.03.29..
+ */
+public class EmailAlreadyExists extends RegisterException {
+
+    public EmailAlreadyExists(String message) {
+        super(message);
+    }
+}
