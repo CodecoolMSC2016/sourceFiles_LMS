50365cb422374731aa00fcae85f2eb2dbcf350f6
imre_meszesan <imre.meszesan@gmail.com>
2017-03-30 18:27:00 +0200

    added setters for name and email

--- src/Users/User.java
+++ src/Users/User.java
@@ -20,4 +20,12 @@ public abstract class User {
     public String getEmail() {
         return email;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
