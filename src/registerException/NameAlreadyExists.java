78c3f8914233de7d52978548e8c2ae4fc0ec9c05
Trixi <bozso.beatrix@gmail.com>
2017-03-30 10:42:07 +0200

    Exception class name modified

--- src/registerException/NameAlreadyExists.java
+++ src/registerException/NameAlreadyExists.java
@@ -3,9 +3,9 @@ package registerException;
 /**
  * Created by Mudzso on 2017.03.29..
  */
-public class NameMissing extends RegisterException {
+public class NameAlreadyExists extends RegisterException {
 
-    public NameMissing(String message) {
+    public NameAlreadyExists(String message) {
         super(message);
     }
 }
