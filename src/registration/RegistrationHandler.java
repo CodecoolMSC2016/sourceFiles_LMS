b14412d7369129d0ff40f5a0395d77534891bad9
imre_meszesan <imre.meszesan@gmail.com>
2017-03-30 16:53:36 +0200

    added path for xml database

--- src/registration/RegistrationHandler.java
+++ src/registration/RegistrationHandler.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 
@@ -18,11 +19,13 @@ import java.io.IOException;
 @WebServlet("/RegistrationHandler")
 public class RegistrationHandler extends HttpServlet {
     private DataContainer container;
-    private ServletConfig config;
+    private String abspath;
 
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         container = DataContainer.getInstance();
+        abspath = "./webapps/sourceFiles_llmmss_war/WEB-INF/classes/registration/registeredUsers.xml";
+        container.loadUsers(abspath);
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String role = request.getParameter("role");
@@ -32,11 +35,11 @@ public class RegistrationHandler extends HttpServlet {
         container = DataContainer.getInstance();
 
         try {
-            container.addUser(name, email, role);
+            container.addUser(name, email, role, abspath);
         } catch (EmailAlreadyExists emailAlreadyExists) {
             request.setAttribute("wrongEmail", emailAlreadyExists.getMessage());
         }
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/register.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("./register.jsp");
         dispatcher.forward(request, response);
     }
 
