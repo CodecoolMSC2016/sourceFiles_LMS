dfb4175d0221df15cd54f9c2e9d9679deea1038d
imre_meszesan <imre.meszesan@gmail.com>
2017-04-10 15:43:32 +0200

    implemented LogoutHandler

---
+++ src/profile/LogoutHandler.java
@@ -0,0 +1,31 @@
+package profile;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+/**
+ * Created by imre_meszesan on 10.04.17.
+ */
+@WebServlet(name = "LogoutHandler",
+    urlPatterns = "/LogoutHandler"
+)
+public class LogoutHandler extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie: cookies){
+                cookie.setMaxAge(0);
+            }
+        }
+        if(session != null){
+            session.invalidate();
+            session.setMaxInactiveInterval(1);
+
+            response.sendRedirect("/login.jsp");
+        }
+    }
+}
