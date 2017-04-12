7b736f8d0dcf4afd418de855bc3581ce3e44c36c
Telakos <tel.akos.2@gmail.com>
2017-04-12 13:59:51 +0200

    started designing curriculum page

--- web/scripts/assingmentLoader.js
+++ web/scripts/assingmentLoader.js
@@ -9,20 +9,38 @@ $(document).ready(function(){
             url: "/CurriculumServlet",
             type: "GET",
             success: function(data){
+                var anchorClass;
+                var type;
+                var scoreSpan;
                 for (var i = 0; i < data.length; i++){
+                    scoreSpan = "";
                     var title = data[i].title;
+                    if (data[i].maxScore == undefined)
+                    {
+                        anchorClass = "button textpage";
+                        type="Text page";
+                    }
+                    else {
+                        anchorClass = "button assignment";
+                        type = "Assignment";
+                        scoreSpan = "<span class=\"meta maxscore\">" + data[i].maxScore + "</span>";
+
+                    }
+
                     var htmlString =
-                        "<a title=\"" + data[i].id + "\" class=\"list-group-item\">";
+                        "<a title=\"" + data[i].id + "\" class=\"" + anchorClass+ "\">" +
+                        "<span class = \"title\">" + title + "</span>" +
+                        "<span class = \"meta type\">" + type + "</span>" + scoreSpan;
 
                     if ($("#role").text() == "mentor"){
 
                         if (!data[i].published){
-                            htmlString += title + "<button title=\"" + data[i].id + "\">PUBLISH</button></a>";
+                            htmlString += "<button style = \"float: right;\" class = \"btn btn-default btn-xs\" title=\"" + data[i].id + "\">Publish</button></a>";
                         }else {
-                            htmlString += title + "<button title=\"" + data[i].id + "\">UNPUBLISH</button></a>";
+                            htmlString += "<button style = \"float: right;\" class = \"btn btn-default btn-xs\" title=\"" + data[i].id + "\">Unpublish</button></a>";
                         }
                     }else {
-                        htmlString += title + "</a></form>";
+                        htmlString += "</a>";
                     }
                     pageContets[data[i].id] = data[i].text;
                     var maxScore = data[i].maxScore;
@@ -47,10 +65,10 @@ $(document).ready(function(){
             console.log(button.text());
             console.log($(this).text());
 
-            if (button.text() == "PUBLISH"){
-                button.text("UNPUBLISH");
+            if (button.text() == "Publish"){
+                button.text("Unpublish");
             }else{
-                button.text("PUBLISH");
+                button.text("Publish");
             }
 
         });
