369ddb696beeeafaa9b3200acdd2c201a7d4d13e
Trixi <bozso.beatrix@gmail.com>
2017-04-12 17:27:53 +0200

    UserListHandler servlet modified for the .jsp

--- src/profile/UserListHandler.java
+++ src/profile/UserListHandler.java
@@ -1,6 +1,7 @@
 package profile;
 
 import Users.User;
+import org.codehaus.jackson.map.ObjectMapper;
 import registration.DataContainer;
 
 import javax.servlet.RequestDispatcher;
@@ -12,6 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -22,35 +26,57 @@ import java.util.Set;
 )
 public class UserListHandler extends HttpServlet {
     DataContainer container;
+    Set<User> studentSet;
+
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         container = DataContainer.getInstance();
+        studentSet = new HashSet<>();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Set<User> registeredUsers = container.getRegisteredUsers();
+
         HttpSession session = request.getSession(false);
         String userRole = (String)session.getAttribute("role");
+
 
         String usersToDisplay = "";
         if (userRole.equals("mentor")){
             for(User user: registeredUsers){
                 String role = user.getClass().getSimpleName();
                 if (role.equals("mentor")){
-                    usersToDisplay += user.getName() + "\n" + role + "\n";
+                    usersToDisplay += "<div class=\"button " + role + "\">" +
+                            "<span class=\"title\">" +
+                            user.getName() + "</span>" +
+                            "<span class=\"meta type\">" +
+                            role +
+                            "</span></div>";
+
                 }else {
-                    usersToDisplay += user + "\n" + role + "\n";
+                    usersToDisplay += "<div class=\"button " + role + "\">" +
+                            "<span class=\"title\">" +
+                            user.getName() + "</span>" +
+                            "<span class=\"meta type\">" +
+                            role +
+                            "<span class=\"meta expiry\">" +
+                            "   Email: " +
+                            user.getEmail() +
+                            "</span></div>";
                 }
             }
         }else {
             for(User user: registeredUsers){
                 String role = user.getClass().getSimpleName();
                 if (role.equals("Student")){
-                    usersToDisplay += user.getName() + "\n" + role + "\n";
-                }else{
-                    usersToDisplay += user.getName() + "\n" + role + "\n";
+                    usersToDisplay += "<div class=\"button " + role + "\">" +
+                            "<span class=\"title\">" +
+                            user.getName() + "</span>" +
+                            "<span class=\"meta type\">" +
+                            role +
+                            "</span></div>";
                 }
             }
         }
