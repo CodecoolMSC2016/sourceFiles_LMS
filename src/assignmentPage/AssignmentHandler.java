6ef7640c33ab071d6d2202e6d2354255bf779e43
Trixi <bozso.beatrix@gmail.com>
2017-04-24 13:57:49 +0200

    added Servlet: Assignmenthandler.java

---
+++ src/assignmentPage/AssignmentHandler.java
@@ -0,0 +1,43 @@
+package assignmentPage;
+
+import Users.User;
+import jdk.nashorn.internal.ir.Assignment;
+import org.codehaus.jackson.map.ObjectMapper;
+import registration.DataContainer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Set;
+
+/**
+ * Created by trixi on 2017.04.24..
+ */
+@WebServlet(name = "AssignmentHandler",
+urlPatterns = "/AssignmentHandler")
+public class AssignmentHandler extends HttpServlet {
+
+    private DataContainer container;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Set<Assignment> assignments = container.getAssignments();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setContentType("application/json");
+        objectMapper.writeValue(response.getOutputStream(), assignments);
+
+
+    }
+}
+
