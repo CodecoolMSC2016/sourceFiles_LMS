7a3a6bfcb8fed5fe4e6f26244eef65577f9358b7
Telakos <tel.akos.2@gmail.com>
2017-04-25 13:51:15 +0200

    Fixed SSL auth. issue in the dbhandler classes

--- src/registration/UserDataBaseHandler.java
+++ src/registration/UserDataBaseHandler.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class UserDataBaseHandler {
-    private static final String DATABASE = "jdbc:mysql://192.168.150.39:3306/LMS";
+    private static final String DATABASE = "jdbc:mysql://192.168.150.39:3306/LMS?useSSL=true";
     private static final String DB_USER = "LMSDBManager";
     private static final String DB_PASSWORD = "szupertitkos";
     private Connection connection;
