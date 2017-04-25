89a2f26c4dc12b2ae18d787b1b0e49fa7c4e1ae8
Telakos <tel.akos.2@gmail.com>
2017-04-25 14:00:30 +0200

    Auto generated test java file added

---
+++ test/CurriculumDataBaseHandlerTest.java
@@ -0,0 +1,50 @@
+import curriculum.CurriculumDataBaseHandler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by akos on 2017.04.25..
+ */
+class CurriculumDataBaseHandlerTest
+{
+    private CurriculumDataBaseHandler cdbh;
+
+    @BeforeAll
+    private void init()
+    {
+        cdbh = CurriculumDataBaseHandler.getInstance();
+    }
+
+    @Test
+    void getCurrciculumDataList()
+    {
+
+    }
+
+    @Test
+    void addAssigmentPage()
+    {
+
+    }
+
+    @Test
+    void addTextPage()
+    {
+
+    }
+
+    @Test
+    void changeIndex()
+    {
+
+    }
+
+    @Test
+    void switchPublished()
+    {
+
+    }
+
+}
